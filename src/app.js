83adfb3f0ea44f12ee9bbdef48f0b254455ccea4
Chris Heald <raythfalstar@gmail.com>
2016-07-19 17:03:55 +0100

    Gameboard reads from a blank initial state

--- src/app.js
+++ src/app.js
@@ -2,15 +2,15 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var GameBoard = require('./components/GameBoard');
-var data = require('./components/data');
+var data = require('./data');
 
 var App = React.createClass({
   getInitialState: function () {
     return {
       boardData: [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null]
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
       ]
     }
   },
@@ -18,10 +18,10 @@ var App = React.createClass({
     return (
       <div>
         <h1>Noughts and Crosses</h1>
-        <GameBoard data={this.state.data} />
+        <GameBoard tilesData={this.state.boardData} />
       </div>
     );
   }
 });
 
-ReactDOM.render(<App data={data}/>, document.getElementById('app'));
+ReactDOM.render(<App/>, document.getElementById('app'));
--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,5 +1,5 @@
 var React = require('react');
-var data = require('./data');
+var data = require('./../data');
 
 var GameBoard = React.createClass({
   render: function () {
@@ -7,19 +7,19 @@ var GameBoard = React.createClass({
       <div>
         <table>
           <tr>
-            <button>{this.props.data.boardData[0][0]}</button>
-            <button>{this.props.data.boardData[0][1]}</button>
-            <button>{this.props.data.boardData[0][2]}</button>
+            <button>{this.props.tilesData[0][0]}</button>
+            <button>{this.props.tilesData[0][1]}</button>
+            <button>{this.props.tilesData[0][2]}</button>
           </tr>
           <tr>
-            <button>{this.props.data.boardData[1][0]}</button>
-            <button>{this.props.data.boardData[1][1]}</button>
-            <button>{this.props.data.boardData[1][2]}</button>
+            <button>{this.props.tilesData[1][0]}</button>
+            <button>{this.props.tilesData[1][1]}</button>
+            <button>{this.props.tilesData[1][2]}</button>
           </tr>
           <tr>
-            <button>{this.props.data.boardData[2][0]}</button>
-            <button>{this.props.data.boardData[2][1]}</button>
-            <button>{this.props.data.boardData[2][2]}</button>
+            <button>{this.props.tilesData[2][0]}</button>
+            <button>{this.props.tilesData[2][1]}</button>
+            <button>{this.props.tilesData[2][2]}</button>
           </tr>
         </table>
       </div>
