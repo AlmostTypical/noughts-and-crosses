635614d051c10b9f507a5079b864cad6587354df
Chris Heald <raythfalstar@gmail.com>
2016-07-20 15:23:18 +0100

    getRow calculates rows correctly

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ var ReactDOM = require('react-dom');
 
 var GameBoard = require('./components/GameBoard');
 var data = require('./data');
+var _ = require('underscore');
 
 var App = React.createClass({
   getInitialState: function () {
@@ -12,22 +13,64 @@ var App = React.createClass({
         ' ', ' ', ' ',
         ' ', ' ', ' '
       ],
-      pFlag: true
+      pFlag: true,
+      winner: ''
     }
   },
-  handleClick: function (id) {
+  getRow: function (e) {
+    var rowOne = this.state.boardData.slice(0, 3);
+    var rowTwo = this.state.boardData.slice(3, 6);
+    var rowThree = this.state.boardData.slice(6);
+    console.log('r1 '+rowOne);
+    console.log('r2 '+rowTwo);
+    console.log('r3 '+rowThree);
+    if (Math.floor(e.target.id/3) === 0) {
+      return rowOne;
+    } else if (Math.floor(e.target.id/3) === 1) {
+      return rowTwo;
+    } else if (Math.floor(e.target.id/3) === 2) {
+      return rowThree;
+    }
+  },
+  getColumn: function (e) {
+    var that = this;
+    console.log(e.target.id);
+    var colOne = this.state.boardData.filter(function(){
+      return that.state.boardData[e.target.id] % 3 === 0});
+    var colTwo =  this.state.boardData.filter(function(){
+      return that.state.boardData[e.target.id] % 3 === 1});
+    var colThree =  this.state.boardData.filter(function(){
+      return that.state.boardData[e.target.id] % 3 === 2});
+    console.log('c1 '+colOne);
+    console.log('c2 '+colTwo);
+    console.log('c3 '+colThree);
+  },
+  handleClick: function (e) {
+    var that = this;
+    var checkWin = function () {
+      var row = that.getRow(e);
+      var col = that.getColumn(e);
+      if (col === ['X', 'X', 'X'] || row === ['X', 'X', 'X']) {
+        that.setState({winner: 'X'})
+      } else if (col === ['O', 'O', 'O'] || row === ['O', 'O', 'O']) {
+        that.setState({winner: 'O'})
+      }
+      console.log(that.state.boardData.winner)
+    };
+
     var newBoard = this.state.boardData.slice();
     var turn = this.state.pFlag;
-    if (this.state.pFlag === true && newBoard[id] === ' ') {
-      newBoard[id] = 'X';
+    if (this.state.pFlag === true && newBoard[e.target.id] === ' ') {
+      newBoard[e.target.id] = 'X';
       this.setState({pFlag: !turn})
-    } else if (newBoard[id] === ' ') {
-      newBoard[id] = 'O';
+    } else if (newBoard[e.target.id] === ' ') {
+      newBoard[e.target.id] = 'O';
       this.setState({pFlag: !turn})
     }
     this.setState({
       boardData: newBoard,
     });
+    checkWin(newBoard[e.target.id]);
   },
   render: function () {
     return (
--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -7,19 +7,19 @@ var GameBoard = React.createClass({
       <div>
         <table>
           <tr>
-            <button onClick={this.props.handleClick.bind(null, 0)} className="tile" id="0">{this.props.tilesData[0]}</button>
-            <button onClick={this.props.handleClick.bind(null, 1)} className="tile" id="1">{this.props.tilesData[1]}</button>
-            <button onClick={this.props.handleClick.bind(null, 2)} className="tile" id="2">{this.props.tilesData[2]}</button>
+            <button onClick={this.props.handleClick} className="tile" id="0">{this.props.tilesData[0]}</button>
+            <button onClick={this.props.handleClick} className="tile" id="1">{this.props.tilesData[1]}</button>
+            <button onClick={this.props.handleClick} className="tile" id="2">{this.props.tilesData[2]}</button>
           </tr>
           <tr>
-            <button onClick={this.props.handleClick.bind(null, 3)} className="tile" id="3">{this.props.tilesData[3]}</button>
-            <button onClick={this.props.handleClick.bind(null, 4)} className="tile" id="4">{this.props.tilesData[4]}</button>
-            <button onClick={this.props.handleClick.bind(null, 5)} className="tile" id="5">{this.props.tilesData[5]}</button>
+            <button onClick={this.props.handleClick} className="tile" id="3">{this.props.tilesData[3]}</button>
+            <button onClick={this.props.handleClick} className="tile" id="4">{this.props.tilesData[4]}</button>
+            <button onClick={this.props.handleClick} className="tile" id="5">{this.props.tilesData[5]}</button>
           </tr>
           <tr>
-            <button onClick={this.props.handleClick.bind(null, 6)} className="tile" id="6">{this.props.tilesData[6]}</button>
-            <button onClick={this.props.handleClick.bind(null, 7)} className="tile" id="7">{this.props.tilesData[7]}</button>
-            <button onClick={this.props.handleClick.bind(null, 8)} className="tile" id="8">{this.props.tilesData[8]}</button>
+            <button onClick={this.props.handleClick} className="tile" id="6">{this.props.tilesData[6]}</button>
+            <button onClick={this.props.handleClick} className="tile" id="7">{this.props.tilesData[7]}</button>
+            <button onClick={this.props.handleClick} className="tile" id="8">{this.props.tilesData[8]}</button>
           </tr>
         </table>
       </div>
